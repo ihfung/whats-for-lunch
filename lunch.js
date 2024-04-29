d78426214d4854407eaae024858b6dc3327d6737
ihleung <ihfungleung@gmail.com>
2024-04-29 15:45:50 -0400

    updated

--- lunch.js
+++ lunch.js
@@ -13,7 +13,7 @@
  * in minutes.
  */
 
-const whatToDoForLunch = function(hungry, availableTime) {
+const whatToDoForLunch = function (hungry, availableTime) {
   //console.log("I don't know what to do!");
   if (!hungry) {
     console.log("Wait until you're hungry.");
